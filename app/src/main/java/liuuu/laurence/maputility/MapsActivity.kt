7433158d36cd64f95870bf34007b28abd4794ce8
laurence-liu <laurence.liuuu@gmail.com>
2018-11-15 00:10:31 +0800

    Add MapController
    setCustomMarker and animateCamera

--- app/src/main/java/liuuu/laurence/maputility/MapsActivity.kt
+++ app/src/main/java/liuuu/laurence/maputility/MapsActivity.kt
@@ -3,24 +3,26 @@ package liuuu.laurence.maputility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var mGoogleMap: GoogleMap
+    private lateinit var mMapController: MapController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        zoomButton.setOnClickListener {
+            mMapController.animateCamera()
+        }
     }
 
     /**
@@ -33,11 +35,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        mGoogleMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMapController = MapController(this, mGoogleMap)
+        mMapController.setCustomMarker()
+
     }
 }
